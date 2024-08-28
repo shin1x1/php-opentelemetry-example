2310d3264933a15a1480cb32bcf2daded0d787e1
shin1x1 <shin1x1@gmail.com>
2024-08-28 23:37:39 +0900

    zero_code_instrumentation: sample api

--- zero_code_instrumentation/app/routes/web.php
+++ zero_code_instrumentation/app/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/contents/{code}', function (string $code) {
+    $content = DB::table('contents')->where('country_code', $code)->first();
+    if ($content === null) {
+        return response()->json(['error' => 'Not Found'], 404);
+    }
+
+    $flag = Http::get('https://restcountries.com/v3.1/name/' . $content->country_code)
+        ->throw()
+        ->json('0.flag');
+
+    return response()->json([
+        'country_code' => $content->country_code,
+        'name' => $content->name,
+        'flag' => $flag,
+    ]);
 });
--- zero_code_instrumentation/readme.md
+++ zero_code_instrumentation/readme.md
@@ -1,3 +1,19 @@
+# PHP + OpenTelemetry ゼロコード計装例
+
+## 環境構築
+
+```shell
+$ make
+```
+
+## PHP コードからトレースデータを送信
+
+http://localhost:8000/
+
+## Jaeger で確認
+
+http://localhost:16686/
+
 ## Usage
 
 ```shell
